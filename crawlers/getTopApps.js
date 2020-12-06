ba6745ea5a6a7e12c4e76117da7e064e03d2e56c
Guilherme Jardim <guilhermejardim@guilhermes-mbp.lan>
2020-12-06 12:34:36 -0400

    getTopCategories implemented

--- crawlers/getTopApps.js
+++ crawlers/getTopApps.js
@@ -1,21 +1,11 @@
 const puppeteer = require("puppeteer");
 const fs = require('fs');
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // abre a página inicial
-    await page.goto("https://play.google.com/store/apps/top");
-
-    const topCategories = await page.evaluate( () => 
-        Array.from(document.querySelectorAll(".xwY9Zc"), (element) => element.querySelector("a").href)
-    );
-
-    console.log(topCategories);
+    
 
     await browser.close();
 })();
---
+++ crawlers/getTopCategories.js
@@ -0,0 +1,39 @@
+const puppeteer = require("puppeteer");
+const fs = require('fs');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // loads top apps main page
+    await page.goto("https://play.google.com/store/apps/top?hl=pt_BR");
+
+    // crawls each top apps category link
+    const topCategoriesLinks = await page.evaluate( () => 
+        Array.from(document.querySelectorAll(".xwY9Zc"), (element) => element.querySelector("a").href)
+    );
+
+    // crawls each top apps category name
+    const topCategoriesNames = await page.evaluate( () => 
+        Array.from(document.querySelectorAll(".xwY9Zc"), (element) => element.querySelector("a").querySelector("h2").innerText)
+    );
+
+    // creates a JSON from names + links
+    var categoriesObj = { data: [] };
+    for (var i in topCategoriesLinks) {
+        var name = topCategoriesNames[i];
+        var link = topCategoriesLinks[i];
+
+        categoriesObj.data.push({
+            "category"  : name,
+            "link"      : link
+        });
+    }
+    const data = JSON.stringify(categoriesObj);
+
+    // saves crawled data
+    fs.writeFileSync("crawled data/linksToCategories.txt", topCategoriesLinks);
+    fs.writeFileSync("crawled data/data.json", data);
+
+    await browser.close();
+})();
