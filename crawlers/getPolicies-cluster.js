43dc302735cde4e0f1b0491f080a2ffe4e77af31
Guilherme Jardim <guilhermejardim@guilhermes-mbp.lan>
2021-04-10 14:22:06 -0400

    refreshed policies

--- crawlers/getPolicies-cluster.js
+++ crawlers/getPolicies-cluster.js
@@ -4,25 +4,28 @@ const puppeteer = require("puppeteer");
 const { Cluster } = require("puppeteer-cluster");
 
 var importedLinks = fs
-    .readFileSync("crawled data/appLinks.txt")
+    .readFileSync("crawled data/allLinks.txt")
     .toString("utf-8")
     .split(",");
 
 (async () => {
     const cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: 8,
+        maxConcurrency: 12,
         monitor: true
     });
+
+    var counter = 0;
 
     const download = async ({ page, data: url }) => {
         await page.setExtraHTTPHeaders({
             'Accept-Language': 'pt'
         });
         await page.goto(url);
-        const pageTitle = await page.evaluate(() => document.title);
+        // const pageTitle = await page.evaluate(() => document.title);
         var data = await page.content();
-        fs.writeFileSync(`crawled data/policies-cluster/${pageTitle}.html`, data);
+        fs.writeFileSync(`crawled data/policies-cluster/page ${counter}.html`, data);
+        counter++;
     };
 
     for (i in importedLinks) {
